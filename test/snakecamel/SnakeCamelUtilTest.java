c5595f2aa6af279f049125046612b2ccbfcae0a7
g16950yi <g16950yi@s30419.tsuda.ac.jp>
2018-04-26 14:33:35 +0900

    エラー修正

--- test/snakecamel/SnakeCamelUtilTest.java
+++ test/snakecamel/SnakeCamelUtilTest.java
@@ -3,12 +3,33 @@ package snakecamel;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
+import static org.hamcrest.CoreMatchers.*;
 public class SnakeCamelUtilTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	
 
+	@Test
+	public void cat_dogTest() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected ="CatDog";
+		String actual = scu.snakeToCamelcase("cat_dog");
+		assertThat(actual,is(expected));
+	}
+	
+	@Test
+	public void cat_dog_fishTest() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected ="CatDogFish";
+		String actual = scu.snakeToCamelcase("cat_dog_fish");
+		assertThat(actual,is(expected));
+	}
+	
+	@Test
+	public void CatDogTest() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected ="cat_dog";
+		String actual = scu.camelToSnakecase("CatDog");
+		assertThat(actual,is(expected));
+	}
+	
 }
